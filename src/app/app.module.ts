1af0cc645086fd37f305ef1aa146480767b53363
Anand-S-L <anandslperoorkada@gmail.com>
2022-08-03 15:50:17 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    ViewEmployeeComponent,
+    SearchEmployeeComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
